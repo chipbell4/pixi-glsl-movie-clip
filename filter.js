49c02f890ddb6d8c59b2775bb8b9bc4360e43ce9
Chip Bell <cbell@pbs.org>
2016-06-28 14:45:52 -0400

    Adding a sprite

--- filter.js
+++ filter.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+PIXI.loader.add('TreetopCity_Zone1_Run1_0', 'TreetopCity_Zone1_Run1_0.png')
+
+PIXI.loader.once('complete', function() {
+  console.log('Loaded');
+
+  var sprite = new PIXI.Sprite(PIXI.loader.resources.TreetopCity_Zone1_Run1_0.texture);
+  stage.addChild(sprite);
+
+  render();
+});
+
+PIXI.loader.load();
