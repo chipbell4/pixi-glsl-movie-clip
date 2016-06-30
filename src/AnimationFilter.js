e877f1d9e4208be081d2dd2de2ea46a686cc8da1
Chip Bell <cbell@pbs.org>
2016-06-30 13:27:10 -0400

    Getting simple iteration over frames

--- src/AnimationFilter.js
+++ src/AnimationFilter.js
@@ -1,4 +1,6 @@
 var AnimationFilter = function() {
+  var TOTAL_FRAMES = 4; // TODO: passed via constructor
+
   var fragmentShader = [
     'precision lowp float;',
 
@@ -8,22 +10,34 @@ var AnimationFilter = function() {
     'uniform sampler2D uSampler;',
     
     // animation things
+    'const int TOTAL_FRAMES =' + TOTAL_FRAMES + ';',
+    'uniform vec4 frames[' + TOTAL_FRAMES + '];',
     'uniform int animationStart;',
     'uniform int currentTime;',
     'uniform int frameRate;',
-    'uniform int totalFrames;',
-    'uniform vec4 frames[256];',
 
     'int getCurrentFrame(void) {',
-    '  float animationDurationInMillis = float(totalFrames) / float(frameRate) * 1000.0;',
+    '  float animationDurationInMillis = float(TOTAL_FRAMES) / float(frameRate) * 1000.0;',
     '  float elapsedMillis = float(currentTime - animationStart);',
     '  float percentageComplete = mod(elapsedMillis, animationDurationInMillis) / animationDurationInMillis;',
-    '  return int(percentageComplete * float(totalFrames));',
+    '  return int(percentageComplete * float(TOTAL_FRAMES));',
+    '}',
+
+    'vec4 selectFrame(const int frameIndex) {',
+    '  vec4 frame = frames[0];',
+    '  for(int i = 0; i < TOTAL_FRAMES; i++) {',
+    '    if(i == frameIndex) {',
+    '      frame = frames[i];',
+    '    }',
+    '  }',
+    '  return frame;',
     '}',
 
     'void main(void){',
-    '  int currentFrame = getCurrentFrame();',
-    '  gl_FragColor = texture2D(uSampler, vTextureCoord) * vColor;',
+    '  int frameIndex = getCurrentFrame();',
+    '  vec4 frame = selectFrame(frameIndex);',
+    '  vec2 frameUV = frame.xy + frame.zw * vTextureCoord;',
+    '  gl_FragColor = texture2D(uSampler, frameUV) * vColor;',
     '}'
   ].join('\n');
 
@@ -35,10 +49,6 @@ var AnimationFilter = function() {
         { x: 0, y: 0.5, z: 0.5, w: 0.5},
         { x: 0.5, y: 0, z: 0.5, w: 0.5}
       ]
-    },
-    totalFrames: {
-      type: 'i',
-      value: 3
     },
     frameRate: {
       type: 'i',
