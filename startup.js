590b74f8b53e5ad08023815a8969d38946398516
Chip Bell <cbell@pbs.org>
2016-06-30 11:45:09 -0400

    FPS calculation

--- startup.js
+++ startup.js
@@ -7,7 +7,23 @@ renderer = PIXI.autoDetectRenderer(800, 600);
 // add the renderer view element to the DOM
 document.body.appendChild(renderer.view);
 
+var lastFrameTime, lastFrameDuration;
 function render() {
+  var now = Date.now();
+  if(lastFrameTime !== undefined) {
+    lastFrameDuration = now - lastFrameTime;
+  }
+  lastFrameTime = now;
+
   renderer.render(stage);
   requestAnimationFrame(render);
 }
+
+var frameRateContainer = document.getElementById('frame-rate');
+setInterval(function() {
+  if(lastFrameDuration == undefined) {
+    return;
+  }
+
+  frameRateContainer.innerHTML = (1000 / lastFrameDuration).toFixed(1);
+}, 1000);
