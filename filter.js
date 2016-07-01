db292f950ac3d233b4e0b178c2dd309801fdbe80
Chip Bell <cbell@pbs.org>
2016-07-01 10:52:25 -0400

    Moving sprite creation to single function, making ticker more robust

--- filter.js
+++ filter.js
@@ -1,16 +1,8 @@
 PIXI.loader.add('TreetopCity_Zone1_Run1_0', 'TreetopCity_Zone1_Run1_0.png')
 PIXI.loader.add('TreetopCity_Zone1_Run1_0_frames', 'TreetopCity_Zone1_Run1_0.json');
 
-PIXI.loader.once('complete', function() {
-  var frames = [];
-  for(var i = 222; i <= 228; i++) {
-    frames.push('Treetop_DDD_Zone1_1_to_' + i + '.png');
-  }
-
-  var parentTexture = PIXI.loader.resources["TreetopCity_Zone1_Run1_0"].texture;
-  var textureMap = PIXI.loader.resources["TreetopCity_Zone1_Run1_0_frames"].data.frames;
-  var animationFilterFrames = AnimationFilter.extractFrameDescriptions(textureMap, frames, parentTexture);
-
+var animators = [];
+var makeSprite = function(animationFilterFrames) {
   var sprite = new PIXI.Sprite(PIXI.loader.resources.TreetopCity_Zone1_Run1_0.texture);
   var aspectRatio = 159 / 210;
   sprite.width = 100;
@@ -23,17 +15,37 @@ PIXI.loader.once('complete', function() {
     viewportDimensions: { x: renderer.width, y: renderer.height },
   });
 
+  animators.push(filter);
+
   sprite.filters = [filter];
 
-
   stage.addChild(sprite);
+
+  return sprite;
+};
+
+PIXI.loader.once('complete', function() {
+  var frames = [];
+  for(var i = 222; i <= 228; i++) {
+    frames.push('Treetop_DDD_Zone1_1_to_' + i + '.png');
+  }
+
+  var parentTexture = PIXI.loader.resources["TreetopCity_Zone1_Run1_0"].texture;
+  var textureMap = PIXI.loader.resources["TreetopCity_Zone1_Run1_0_frames"].data.frames;
+  var animationFilterFrames = AnimationFilter.extractFrameDescriptions(textureMap, frames, parentTexture);
+
+  makeSprite(animationFilterFrames);
 
   render();
 
   var tick = function() {
-    filter.tick();
+    animators.forEach(function(animator) {
+      animator.tick();
+    });
+
     requestAnimationFrame(tick);
   };
+
   tick();
 });
 
